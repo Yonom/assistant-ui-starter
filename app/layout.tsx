fb2578afb258f7f15ca5d3fad436b755356602e8
Simon Farshid <simon.farshid@outlook.com>
2024-06-20 19:31:29 -0700

    setup ai sdk runtime

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { MyRuntimeProvider } from "@/app/MyRuntimeProvider";
+import { cn } from "@/lib/utils";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <MyRuntimeProvider>
+      <html lang="en" className="h-full">
+        <body className={cn(inter.className, "h-full")}>{children}</body>
+      </html>
+    </MyRuntimeProvider>
   );
 }
