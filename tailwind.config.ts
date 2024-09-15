a718a816deaae3f7c47d2e3ebf2e08ee08d3aa48
Simon Farshid <simon.farshid@outlook.com>
2024-09-14 21:00:18 -0700

    directly pass runtime to Thread, update template

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,9 @@ import type { Config } from "tailwindcss";
 const config = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   plugins: [
     require("tailwindcss-animate"),
